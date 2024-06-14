f933142970e3825de738f79bd8747f6a6a89c238
v-rymashevsky <rymashevskiy.v@gmail.com>
2024-06-14 22:21:39 +0400

    added header component

---
+++ src/components/header/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+interface HeaderProps { }
+
+export const Header: React.FC<HeaderProps> = () => {
+  // const switchLinkState = ({ isActive }: { isActive: boolean }): string =>
+  //   isActive ? 'nav-a active px-3' : 'nav-a px-3'
+
+  return (
+    <nav className="navbar bg-primary" data-bs-theme="dark">
+      <div className="container-fluid">
+        <span className="navbar-brand">Bookstore</span>
+        <form className="d-flex align-items-center ms-5" role="search">
+          <input type="search" className="form-control me-1" placeholder="Search..." style={{ width: '200px' }}/>
+          <button type="submit" className="btn btn-warning"><i className="bi bi-search"></i></button>
+        </form>
+        <div className="navbar-nav flex-row">
+          <a className="nav-link px-3" aria-current="page" href="#">Favourites</a>
+          <a className="nav-link px-3" href="#">Cart</a>
+          <a className="nav-link px-3" href="#">User</a>
+        </div>
+      </div>
+    </nav>
+  )
+}
