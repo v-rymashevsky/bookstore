3659cca911f156823e616d9bd3b269365280fa49
v-rymashevsky <rymashevskiy.v@gmail.com>
2024-06-19 16:19:36 +0400

    added pagination to search results page

---
+++ src/components/pagination/index.tsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+interface PaginationProps {
+  currentPage: number
+  pagesCount: number
+  to: string
+}
+
+export const Pagination: React.FC<PaginationProps> = ({ currentPage, pagesCount, to }) => {
+  function buildPaginationScheme () {
+    const prevPageNumber = currentPage - 1
+    const nextPageNumber = currentPage + 1
+    const scheme = [1, prevPageNumber, currentPage, nextPageNumber, pagesCount]
+    const filteredScheme = scheme.filter(item => item > 0 && item <= pagesCount)
+    const set = new Set(filteredScheme)
+    const result: (number | string)[] = Array.from(set)
+
+    if (Number(result[0]) + 1 !== result[1]) result.splice(1, 0, '...')
+    if (Number(result[result.length - 2]) + 1 !== result[result.length - 1]) {
+      result.splice(result.length - 1, 0, '...')
+    }
+    return result
+  }
+
+  const paginationScheme = buildPaginationScheme()
+
+  if (pagesCount === 1) return null
+
+  return (
+    <ul className="pagination d-flex justify-content-start mt-auto">
+      {paginationScheme.map((item, index) => {
+        if (typeof item === 'number') {
+          return (
+            <li className="page-item" key={index}>
+              <NavLink className="page-link" to={`/${to}${item}`}>
+                {item}
+              </NavLink>
+            </li>
+          )
+        }
+        return (
+          <li className="page-item" key={index}>
+            <span className="page-link">...</span>
+          </li>
+        )
+      })}
+    </ul>
+  )
+}
--- src/pages/SearchResults.tsx
+++ src/pages/SearchResults.tsx
@@ -2,11 +2,11 @@ import React, { useEffect } from 'react'
 import { useSelector } from 'react-redux'
 import { useParams } from 'react-router-dom'
 import { Title } from '../components/title'
-// import { Pagination } from '../components/pagination'
+import { RootState, useAppDispatch } from '../redux/store'
 import { Book, fetchSearchResults } from '../redux/books-slice'
 import { Spinner } from '../components/spinner'
 import { Card } from '../components/card'
-import { RootState, useAppDispatch } from '../redux/store'
+import { Pagination } from '../components/pagination'
 
 export const SearchResults: React.FC = () => {
   const dispatch = useAppDispatch()
@@ -15,6 +15,7 @@ export const SearchResults: React.FC = () => {
   const books = useSelector((state: RootState) => state.books.list)
   const error = useSelector((state: RootState) => state.books.error)
   const isLoading = useSelector((state: RootState) => state.books.isLoading)
+  const pagesCount = useSelector((state: RootState) => state.books.pagesCount)
 
   useEffect(() => {
     dispatch(fetchSearchResults({ search: query, page: page || '1' }))
@@ -37,11 +38,12 @@ export const SearchResults: React.FC = () => {
   }
 
   return (
-        <>
-            <Title>Search results for  &quot;{query}&quot;</Title>
-            <div className="d-flex flex-wrap gap-2 mb-3">
-                {renderBooks()}
-            </div>
-        </>
+    <>
+      <Title>Search results for  &quot;{query}&quot;</Title>
+      <Pagination pagesCount={pagesCount} currentPage={Number(page)} to={`search/${query}/`} />
+      <div className="d-flex flex-wrap gap-2 mb-3">
+        {renderBooks()}
+      </div>
+    </>
   )
 }
